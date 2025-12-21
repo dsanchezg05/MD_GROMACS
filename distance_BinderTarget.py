213b5ab23dda29a112b5745dae6b7e43642c16c0
dsanchezg05 <108696475+dsanchezg05@users.noreply.github.com>
2025-12-21 18:53:42 +0100

    Update distance_BinderTarget.py

--- distance_BinderTarget.py
+++ distance_BinderTarget.py
@@ -3,6 +3,7 @@ import argparse
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import statistics
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--directory", "-d", help='Working directory')
@@ -14,4 +15,8 @@ df=pd.read_csv(f"{dir}/distance.csv", header=None)
 print(df.head())
 print(df.iloc)
 plt.plot(np.array(df.iloc[:,0]), np.array(df.iloc[:,1]))
+plt.xlabel("Time")
+plt.ylabel("RMSD")
+mean_rmsd=statistics.mean(np.array(df.iloc[:,1]))
+plt.title(f"Distance Binder vs Target, mean RMSD={round(mean_rmsd,3)}")
 plt.show()
